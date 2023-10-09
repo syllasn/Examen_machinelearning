d05da2d89dcf19c694dd45d21be3026e4e07bdc2
billfass <billfass2010@gmail.com>
2023-10-09 20:30:32 +0100

    fonction train model

--- functions.py
+++ functions.py
@@ -1,28 +1,34 @@
 # import libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.model_selection import ParameterGrid
+from sklearn.metrics import mean_squared_error
 
 # Function to load data (5 pts)
 def load_data(file_path):
     # Load data from the CSV file or another format and return data
-    data = pd.read_excel(file_path)
+    data = pd.read_excel(file_path, sheet_name='Feuille 1')
     return data
 
 # Function to preprocess data (handling missing and outlier data) (15 pts)
-def preprocess_data(data):
+def preprocess_data(file_path):
     # Handle missing data using appropriate imputation
     # Deal with outlier data 
     # return data
-    
+    # Read the data from the input file into a DataFrame
+    data = load_data(file_path)
+
     # Drop any rows with missing values
     data.dropna(inplace=True)
 
     # Convert the 'date' column to a datetime object
     data['date'] = pd.to_datetime(data['date'])
 
-    # Encode categorical variables (Facility) using one-hot encoding
-    data = pd.get_dummies(data, columns=['Facility'], drop_first=True)
-
+    # define 'date' as index
+    data.set_index('date', inplace=True)
     return data
 
 # Function to split data into training and testing sets (5 pts)
@@ -38,9 +44,41 @@ def split_data(preprocessed_data):
 
 # Function to train a model with hyperparameters (30 pts)
 def train_model(X_train, y_train): 
-    # Train a or many models with hyperparameter tuning
-    # Return best model
-    pass
+    # Define a grid of hyperparameters to search through
+    param_grid = {
+        'p': range(0, 3),  # AR order
+        'd': range(0, 2),  # I order
+        'q': range(0, 3),  # MA order
+        'P': range(0, 3),  # Seasonal AR order
+        'D': range(0, 2),  # Seasonal I order
+        'Q': range(0, 3),  # Seasonal MA order
+        's': [12],         # Seasonal period (assuming monthly data)
+    }
+
+    best_model = None
+    best_mse = float('inf')
+
+    # Iterate through all possible combinations of hyperparameters
+    for params in ParameterGrid(param_grid):
+        try:
+            # Fit a SARIMA model with the current hyperparameters
+            model = SARIMAX(y_train, exog=X_train, order=(params['p'], params['d'], params['q']),
+                            seasonal_order=(params['P'], params['D'], params['Q'], params['s']))
+            results = model.fit()
+
+            # Calculate Mean Squared Error (MSE)
+            mse = mean_squared_error(y_train, results.fittedvalues)
+
+            # Update the best model if this one has a lower MSE
+            if mse < best_mse:
+                best_mse = mse
+                best_model = results
+        except:
+            continue
+
+    # The best model is not returned, it's available for use outside of this function
+    return
+
 
 # Function to evaluate the model (15 pts)
 def evaluate_model(model, X_test, y_test):
